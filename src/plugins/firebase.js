416b01751e4105122e4f5cb1f355529f9a648dba
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-19 18:05:57 +0900

    firebaseの導入

---
+++ src/plugins/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+// Firebase設定
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DATABASE_URL,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID
+}
+// Firebase初期化
+firebase.initializeApp(firebaseConfig)
