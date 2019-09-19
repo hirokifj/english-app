16e093b78876433ec343e10915ee2b6101863dc6
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-19 20:10:05 +0900

    共通CSS、ユーティリティークラスの読み込み

--- src/App.vue
+++ src/App.vue
@@ -17,21 +17,51 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*,
+*::after,
+*::before {
+	margin: 0;
+	padding: 0;
+	box-sizing: inherit;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+html {
+	box-sizing: border-box;
+	font-size: 62.5%;	//1 rem = 10px; 10px/16px = 62.5%
+
+	@include respond(tab-land) {
+		font-size: 56.25%;
+	}
+
+	@include respond(tab-port) {
+		font-size: 50%;
+	}
+}
+
+body {
+  font-size: $default-font-size;
+  font-family: -apple-system, Segoe UI, Helvetica Neue,Hiragino Kaku Gothic ProN, "メイリオ", meiryo, sans-serif;
+  font-weight: 400;
+  line-height: 1.8;
+  color: $color-grey-dark-3;
+  -webkit-font-smoothing: antialiased;
+}
+
+.container {
+	max-width: 100%;
+	margin: 0 0;
+}
+
+.link-text {
+  transition: color .4s;
+
+  &:link,
+  &:visited {
+    color: $color-blue;
+  }
+
+  &:hover {
+    color: $color-green;
   }
 }
 </style>
