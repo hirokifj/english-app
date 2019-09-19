730357cce9c438dd946f0136eb7ebe7c7104044f
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-19 21:21:02 +0900

    ヘッダーコンポーネントの作成

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <div class="header__container u-max-width">
+      <h1 class="site-title">
+        <router-link :to="{ name: 'home' }">English App</router-link>
+      </h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: $color-blue;
+  color: $color-white;
+  padding: 1rem 0;
+
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+.site-title {
+  font-size: 2.4rem;
+  font-weight: bold;
+  letter-spacing: 2px;
+
+  & a {
+    transition: color .4s;
+
+    &:link,
+    &:visited {
+      color: $color-white;
+      text-decoration: none;
+    }
+
+    &:hover {
+      color: $color-pink-light;
+    }
+  }
+}
+</style>
