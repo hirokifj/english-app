ef0bbfed3224771653fa42af4c1c60815f6b79ce
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-20 16:34:03 +0900

    ログアウト処理を追加

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -1,12 +1,43 @@
 <template>
   <main class="main">
     <div class="u-max-width">
-      <button class="btn btn--yellow">
+
+      <ErrMsg class="u-mb-medium" />
+
+      <button class="btn btn--yellow" @click="signOut">
         ログアウト
       </button>
     </div>
   </main>
 </template>
+
+<script>
+import firebase from 'firebase'
+import ErrMsg from '../components/ErrMsg'
+
+export default {
+  components: {
+    ErrMsg
+  },
+  methods: {
+    async signOut() {
+      // エラーメッセージ初期化
+      this.$store.dispatch('error/clearError')
+
+      try {
+        // Firebaseからのログアウト
+        await firebase.auth().signOut()
+        // storeのログインユーザー情報をクリア
+        this.$store.dispatch('user/clearLoginUser')
+
+        this.$router.push({ name: 'signin' })
+      } catch(error) {
+        this.$store.dispatch('error/setError', error)
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .main {
