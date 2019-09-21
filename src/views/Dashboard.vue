6262a8a2a9edbc16fdfc545a87068243bcd6a0d2
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-21 23:26:18 +0900

    ダッシュボードへの例文一覧表示を実装

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,77 @@
 <template>
   <main class="main">
     <div class="u-max-width">
-      <router-link :to="{ name: 'senetncesCreate' }" class="btn btn--green btn--small">例文登録</router-link>
+
+      <ErrMsg class="u-mb-medium" />
+
+      <div class="btn-group u-mb-big">
+        <router-link :to="{ name: 'senetncesCreate' }" class="btn btn--green btn--small">例文登録</router-link>
+      </div>
+      <div class="card-group">
+        <Card color="green" :white="true">
+          <template slot="header">
+            登録した例文
+          </template>
+          <SentencesList :sentences="sentences" class="u-mb-medium" />
+          <div class="u-center-text">
+            <router-link :to="{ name: 'senetncesList' }" class="btn btn--green btn--big">もっと見る</router-link>
+          </div>
+        </Card>
+      </div>
     </div>
   </main>
 </template>
+
+<script>
+import SentencesList from '../components/SentencesList'
+import Card from '../components/Card'
+import ErrMsg from '../components/ErrMsg'
+import { fetchUserSentences } from '../lib/functions'
+
+export default {
+  async created() {
+    // 例文一覧の取得
+    const sentencesResults = await fetchUserSentences(5, this.loginUser.id)
+    sentencesResults.items.forEach(item => {
+      this.sentences.push(item)
+    })
+  },
+  data() {
+    return {
+      sentences: []
+    }
+  },
+  components: {
+    SentencesList,
+    Card,
+    ErrMsg
+  },
+  computed: {
+    loginUser() {
+      return this.$store.state.user.loginUser
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .main {
   padding: 8rem 0 12rem 0;
   background-color: $color-bg-grey;
+}
+
+.card-group {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  @include respond(tab-port) {
+    display: block;
+  }
+
+  & .card {
+    flex: 0 0 45%;
+    margin-bottom: 6rem;
+  }
 }
 </style>
