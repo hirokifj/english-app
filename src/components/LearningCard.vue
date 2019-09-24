284e241e8feb29b02278095f0e4d73558493e4a3
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-24 15:48:41 +0900

    学習モード用例文表示コンポーネントを実装

---
+++ src/components/LearningCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="learning-card">
+    <span class="english">{{ currentSentence.english }}</span>
+    <span class="japanese">{{ currentSentence.japanese }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    learningSentences: Array
+  },
+  data() {
+    return {
+      currentNum: 0
+    }
+  },
+  computed: {
+    currentSentence() {
+      return this.learningSentences[this.currentNum]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.learning-card {
+  border: 2px solid $color-line-grey;
+  border-radius: 5px;
+  padding: 4rem 2rem;
+}
+
+.english {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 2rem;
+}
+.japanese {
+  font-size: 14px;
+}
+</style>
