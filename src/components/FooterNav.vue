129648b65b8d8b71d3b08e108d452c1180256856
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-09-28 17:13:47 +0900

    フッター追従ナビにリンクを追加

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,6 +1,22 @@
 <template>
   <nav class="footer-menu">
     <ul class="footer-menu__list">
+      <li class="footer-menu__item">
+        <router-link :to="{ name: 'ranking' }" class="footer-menu__link">
+          <div class="footer-menu__icon">
+            <font-awesome-icon icon="crown" />
+          </div>
+          <span class="footer-menu__text">ランキング</span>
+        </router-link>
+      </li>
+      <li class="footer-menu__item">
+        <router-link :to="{ name: 'listsList' }" class="footer-menu__link">
+          <div class="footer-menu__icon">
+            <font-awesome-icon icon="list" />
+          </div>
+          <span class="footer-menu__text">リスト</span>
+        </router-link>
+      </li>
       <template v-if="loginUser">
         <li class="footer-menu__item">
           <router-link :to="{ name: 'dashboard' }" class="footer-menu__link">
@@ -17,7 +33,7 @@
             <div class="footer-menu__icon">
               <font-awesome-icon icon="user-plus" />
             </div>
-            <span class="footer-menu__text">ユーザー登録</span>
+            <span class="footer-menu__text">登録</span>
           </router-link>
         </li>
         <li class="footer-menu__item">
